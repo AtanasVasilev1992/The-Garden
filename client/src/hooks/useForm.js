e7db1a64f09ccad87be05220da7cbeaf471d9090
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-08-05 16:32:47 +0300

    Create hook useForm

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export function useForm(initialValues, submitCallback, reinitializeForm = false ) {
+    const [ values, setValues ] = useState(initialValues);
+
+    useEffect(() => {
+        if(reinitializeForm){
+            setValues(initialValues);
+        }
+    }, [initialValues, reinitializeForm]);
+
+    const changeHandler = (e) => {
+        setValues( state => ({
+            ...state,
+            [e.target.name] : e.target.value
+        }))
+    }
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        await submitCallback(values);
+        
+        setValues(initialValues);
+    }
+
+    return {
+        values,
+        changeHandler,
+        submitHandler,
+    };
+}
