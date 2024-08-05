3ea3cb7a7365d9ffb5673a9d53423626dbf02568
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-08-05 16:51:49 +0300

    Change authContext from js to jsx

--- client/src/HOC/withAuth.js
+++ client/src/HOC/withAuth.js
@@ -1,4 +1,5 @@
-import { useAuthContext } from "../contexts/AuthContext";
+import { useAuthContext } from "../context/authContext";
+
 
 export default function withAuth(Component) {
     const ComponnentWrapper = (props) => {
