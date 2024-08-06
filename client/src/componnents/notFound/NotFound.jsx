e81bc42d573fa3816bc16b068a14e6c6ef5c111a
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-08-06 17:54:52 +0300

    Add Page not found

---
+++ client/src/componnents/notFound/NotFound.jsx
@@ -0,0 +1,17 @@
+export default function NotFound() {
+    return (
+        <>
+        <div className="container-fluid bg-primary py-5 bg-hero mb-5">
+                <div className="container py-5">
+                    <div className="row justify-content-start">
+                        <div className="col-lg-8 text-center text-lg-start">
+                            <h2 className="display-1 text-white mb-md-4">This Page Is Not Found!</h2>
+                            <Link to="/" className="btn btn-primary py-md-3 px-md-5 me-3">Home</Link>
+                            <Link to="/vegetables/create" className="btn btn-secondary py-md-3 px-md-5">Add New</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- client/src/componnents/vegetables/Vegetables.jsx
+++ client/src/componnents/vegetables/Vegetables.jsx
@@ -4,13 +4,13 @@ import VegetableCard from "./card/VegetableCard";
 export default function Vegetables() {
     return (
         <>
-            <div class="container-fluid bg-primary py-5 bg-hero mb-5">
-                <div class="container py-5">
-                    <div class="row justify-content-start">
-                        <div class="col-lg-8 text-center text-lg-start">
-                            <h1 class="display-1 text-white mb-md-4">Vegetables</h1>
-                            <Link to="/" class="btn btn-primary py-md-3 px-md-5 me-3">Home</Link>
-                            <Link to="/vegetables/create" class="btn btn-secondary py-md-3 px-md-5">Add New</Link>
+            <div className="container-fluid bg-primary py-5 bg-hero mb-5">
+                <div className="container py-5">
+                    <div clasNames="row justify-content-start">
+                        <div className="col-lg-8 text-center text-lg-start">
+                            <h1 className="display-1 text-white mb-md-4">Vegetables</h1>
+                            <Link to="/" className="btn btn-primary py-md-3 px-md-5 me-3">Home</Link>
+                            <Link to="/vegetables/create" className="btn btn-secondary py-md-3 px-md-5">Add New</Link>
                         </div>
                     </div>
                 </div>
