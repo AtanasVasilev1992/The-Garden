64ffc3dfb536caaa793487ce8d06a7a8819277dc
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-08-06 13:59:15 +0300

    Remove unnecessary files from app

--- client/src/componnents/home/Home.jsx
+++ client/src/componnents/home/Home.jsx
@@ -11,33 +11,14 @@ export default function Home() {
                             <img className="w-100" src="img/carousel-1.jpg" alt="Image" />
                             <div className="carousel-caption top-0 bottom-0 start-0 end-0 d-flex flex-column align-items-center justify-content-center">
                                 <div className="text-start p-5" style={{ maxWidth: '900px' }}>
-                                    <h3 className="text-white">Organic Vegetables</h3>
-                                    <h1 className="display-1 text-white mb-md-4">Organic Vegetables For Healthy Life</h1>
-                                    <Link to={"/features"} className="btn btn-primary py-md-3 px-md-5 me-3">Explore</Link>
-                                    <Link to={"/team"} className="btn btn-secondary py-md-3 px-md-5">Contact</Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img className="w-100" src="img/carousel-2.jpg" alt="Image" />
-                            <div className="carousel-caption top-0 bottom-0 start-0 end-0 d-flex flex-column align-items-center justify-content-center">
-                                <div className="text-start p-5" style={{ maxWidth: '900px' }}>
-                                    <h3 className="text-white">Organic Fruits</h3>
-                                    <h1 className="display-1 text-white mb-md-4">Organic Fruits For Better Health</h1>
-                                    <Link to={"/features"} className="btn btn-primary py-md-3 px-md-5 me-3">Explore</Link>
-                                    <Link to={"/team"} className="btn btn-secondary py-md-3 px-md-5">Contact</Link>
+                                    <h3 className="text-white">Produces</h3>
+                                    <h1 className="display-1 text-white mb-md-4">Organic Produces For Healthy Life</h1>
+                                    <Link to={"/vegetables"} className="btn btn-primary py-md-3 px-md-5 me-3">Vegetables</Link>
+                                    <Link to={"/fruits"} className="btn btn-secondary py-md-3 px-md-5">Fruits</Link>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#header-carousel" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="" />
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#header-carousel" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="" />
-                        <span className="visually-hidden">Next</span>
-                    </button>
                 </div>
             </div>
             <Banner />
