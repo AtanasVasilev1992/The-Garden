46d0a34fef00e38aa0b91583576b70cf97e38a8e
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-08-07 10:31:22 +0300

    Show dymanic vegetable view

--- client/src/componnents/detailsFruit/DetailsFruit.jsx
+++ client/src/componnents/detailsFruit/DetailsFruit.jsx
@@ -36,9 +36,9 @@ export default function DetailsFruit() {
     const isOwner = userId === fruit._ownerId;
 
     const fruitDeleteHandler = async () => {
-        const isConfirm = confirm(`Are you sure you want delete game: ${fruit.title} ?`);
+        const isConfirm = confirm(`Are you sure you want delete fruit: ${fruit.title} ?`);
 
-        if(!isConfirm) {
+        if (!isConfirm) {
             return
         };
 
@@ -48,7 +48,7 @@ export default function DetailsFruit() {
             navigate('/');
         } catch (err) {
             console.log(err.message);
-            
+
         }
     };
     return (
@@ -57,20 +57,20 @@ export default function DetailsFruit() {
                 <div className="row g-5">
                     <div className="col-lg-8">
                         {/* Blog Detail Start */}
-                        <div className="mb-5">
+                        <div className="mb-5" style={{ border: '2px solid #f93', padding: '2em' }}>
                             <div className="row g-5 mb-5">
                                 <div className="col-md-6">
-                                    <img className="img-fluid w-100" src={fruit.imageUrl} alt="" />
+                                    <img className="img-fluid w-100" src={fruit.imageUrl} alt="This is fruit" />
                                 </div>
                             </div>
                             <h1 className="mb-4">{fruit.title}</h1>
                             <p>{fruit.description}</p>
                             {isOwner &&
-                    (<div className="buttons">
-                        <Link to={`/fruits/${fruitId}/edit`}className="button">Edit</Link>
-                        <a href="#" onClick={fruitDeleteHandler} className="button">Delete</a>
-                    </div>
-                    )}
+                                (<div >
+                                    <Link to={`/fruits/${fruitId}/edit`} className="btn btn-primary py-md-3 px-md-5 me-3">Edit</Link>
+                                    <Link to="#" className="btn btn-secondary py-md-3 px-md-5" onClick={fruitDeleteHandler}>Delete</Link>
+                                </div>
+                                )}
                         </div>
                         {/* Blog Detail End */}
                         {/* Comment List Start */}
@@ -97,45 +97,45 @@ export default function DetailsFruit() {
                             </div>
                         </div>
                         {/* Comment List End */}
-                        
+
                         {/* Comment Form Start */}
                         {isAuthenticated && (
-                        <div className="bg-primary p-5">
-                            <h2 className="text-white mb-4">Leave a comment</h2>
-                            <form>
-                                <div className="row g-3">
-                                    <div className="col-12 col-sm-6">
-                                        <input
-                                            type="text"
-                                            className="form-control bg-white border-0"
-                                            placeholder="Your Name"
-                                            style={{ height: 55 }}
-                                        />
+                            <div className="bg-primary p-5">
+                                <h2 className="text-white mb-4">Leave a comment</h2>
+                                <form>
+                                    <div className="row g-3">
+                                        <div className="col-12 col-sm-6">
+                                            <input
+                                                type="text"
+                                                className="form-control bg-white border-0"
+                                                placeholder="Your Name"
+                                                style={{ height: 55 }}
+                                            />
+                                        </div>
+                                        <div className="col-12 col-sm-6">
+                                            <input
+                                                type="email"
+                                                className="form-control bg-white border-0"
+                                                placeholder="Your Email"
+                                                style={{ height: 55 }}
+                                            />
+                                        </div>
+                                        <div className="col-12">
+                                            <textarea
+                                                className="form-control bg-white border-0"
+                                                rows={5}
+                                                placeholder="Comment"
+                                                defaultValue={""}
+                                            />
+                                        </div>
+                                        <div className="col-12">
+                                            <button className="btn btn-secondary w-100 py-3" type="submit">
+                                                Leave Your Comment
+                                            </button>
+                                        </div>
                                     </div>
-                                    <div className="col-12 col-sm-6">
-                                        <input
-                                            type="email"
-                                            className="form-control bg-white border-0"
-                                            placeholder="Your Email"
-                                            style={{ height: 55 }}
-                                        />
-                                    </div>
-                                    <div className="col-12">
-                                        <textarea
-                                            className="form-control bg-white border-0"
-                                            rows={5}
-                                            placeholder="Comment"
-                                            defaultValue={""}
-                                        />
-                                    </div>
-                                    <div className="col-12">
-                                        <button className="btn btn-secondary w-100 py-3" type="submit">
-                                            Leave Your Comment
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                                </form>
+                            </div>
                         )}
                         {/* Comment Form End */}
                     </div>
--- client/src/componnents/detailsVegetable/DetailsVegetable.jsx
+++ client/src/componnents/detailsVegetable/DetailsVegetable.jsx
@@ -1,18 +1,74 @@
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useAuthContext } from "../../context/authContext";
+import { useGetOneVegetables } from "../../hooks/useVegetables";
+import { useForm } from "../../hooks/useForm";
+import vegetablesApi from "../../api/vegetables-api";
+
+const initialValues = {
+    comment: ''
+}
+
 export default function DetailsVegetable() {
+    const navigate = useNavigate();
+    const { vegetableId } = useParams();
+    // const [commet, dispatchComments] = useGetAllComennts()
+    // const createComment = useCreateComment();
+    const { isAuthenticated, username, userId } = useAuthContext()
+    const [vegetable] = useGetOneVegetables(vegetableId);
+    const {
+        changeHandler,
+        submitHandler,
+        values,
+    } = useForm(initialValues, async ({ comment }) => {
+        try {
+            const newComment = await createComment(fruitId, comment);
+
+            dispatchComments({ type: 'ADD_COMMENT', payload: { ...newComment, author: { username } } })
+        } catch (err) {
+            console.log(err.message);
+
+        }
+    });
+
+    const isOwner = userId === vegetable._ownerId;
+
+    const vegetableDeleteHandler = async () => {
+        const isConfirm = confirm(`Are you sure you want delete vegetable: ${vegetable.title} ?`);
+
+        if(!isConfirm) {
+            return
+        };
+
+        try {
+            await vegetablesApi.remove(vegetableId);
+
+            navigate('/');
+        } catch (err) {
+            console.log(err.message);
+            
+        }
+    };
+
     return (
         <>
          <div className="container py-5">
                 <div className="row g-5">
                     <div className="col-lg-8">
                         {/* Blog Detail Start */}
-                        <div className="mb-5">
+                        <div className="mb-5" style={{border: '2px solid #34AD54', padding: '2em'}}>
                             <div className="row g-5 mb-5">
                                 <div className="col-md-6">
-                                    <img className="img-fluid w-100" src="../../../img/blog-1.jpg" alt="" />
+                                    <img className="img-fluid w-100" src={vegetable.imageUrl} alt="This is vegetable" />
                                 </div>
                             </div>
-                            <h1 className="mb-4">Cucumber</h1>
-                            <p>The best</p>
+                            <h1 className="mb-4">{vegetable.title}</h1>
+                            <p>{vegetable.description}</p>
+                            {isOwner &&
+                                (<div >
+                                    <Link to={`/vegetables/${vegetableId}/edit`} className="btn btn-primary py-md-3 px-md-5 me-3">Edit</Link>
+                                    <Link to="#" className="btn btn-secondary py-md-3 px-md-5" onClick={vegetableDeleteHandler}>Delete</Link>
+                                </div>
+                                )}
                         </div>
                         {/* Blog Detail End */}
                         {/* Comment List Start */}
@@ -40,6 +96,7 @@ export default function DetailsVegetable() {
                         </div>
                         {/* Comment List End */}
                         {/* Comment Form Start */}
+                        {isAuthenticated && (
                         <div className="bg-primary p-5">
                             <h2 className="text-white mb-4">Leave a comment</h2>
                             <form>
@@ -76,6 +133,7 @@ export default function DetailsVegetable() {
                                 </div>
                             </form>
                         </div>
+                        )}
                         {/* Comment Form End */}
                     </div>
                 </div>
