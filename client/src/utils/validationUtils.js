178324bc2dd0dd41453ac9cf48692a9b58874e6c
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2025-01-01 22:03:23 +0200

    Add Validation functions in validationUtils.js

--- client/src/utils/validationUtils.js
+++ client/src/utils/validationUtils.js
@@ -40,3 +40,83 @@ const VALIDATION_MESSAGES = {
 }
 
 // Validation functions
+export const validateField = (name, value, rules = {}) => {
+    const errors = {};
+
+    if (rules.required && !value.trim()) {
+        errors.push(VALIDATION_MESSAGES.required(name));
+    };
+
+    if (rules.minLength && value?.length < rules?.minLength) {
+        errors.push(VALIDATION_MESSAGES.minLength(name, rules.maxLength));
+    };
+
+    if (rules.maxLength && value?.length > rules.maxLength) {
+        errors.push(VALIDATION_MESSAGES.maxLength(name, rules.maxLength));
+    };
+
+    if (rules.pattern && !rules.pattern.test(value)) {
+        errors.push(VALIDATION_MESSAGES.pattern[name]);
+    };
+
+    return errors;
+};
+
+// Form validations 
+export const validateProductForm = (values) => {
+    const errors = {};
+
+    const titleErrors = validateField('Title', values.title, VALIDATION_RULES.title);
+    if (titleErrors.length) {
+        errors.title = titleErrors;
+    };
+
+    const descriptionErrors = validateField('Description', values.description, VALIDATION_RULES.description);
+    if (descriptionErrors.length) {
+        errors.description = descriptionErrors;
+    };
+
+    const imageUrlErrors = validateField('Image Url', values.imageUrl, VALIDATION_RULES.imageUrl);
+    if (imageUrlErrors.length) {
+        errors.imageUrl = imageUrlErrors;
+    };
+
+    return errors;
+};
+
+export const validateLoginForm = (values) => {
+    const errors = {};
+
+    const emailErrors = validateField('Email', values.email, VALIDATION_RULES.email);
+    if (emailErrors.length) {
+        errors.email = emailErrors;
+    };
+
+    const passwordErrors = validateField('Password', values.password, VALIDATION_RULES.password);
+    if (passwordErrors.length) {
+        errors.password = passwordErrors;
+    };
+
+    return errors;
+};
+
+export const validateRegisterForm = (values) => {
+    const errors = {};
+
+    if (values.password != values.rePassword) {
+        errors.rePassword = [VALIDATION_MESSAGES.passwordMatch];
+    };
+
+    return errors;
+};
+
+export const validateCommentForm = (values) => {
+    const errors = {};
+
+    const commentsErrors = validateField('Comment', values.comment, VALIDATION_RULES.comment);
+    if (commentsErrors.length) {
+        errors.comment = commentsErrors;
+    }
+
+    return errors;
+}
