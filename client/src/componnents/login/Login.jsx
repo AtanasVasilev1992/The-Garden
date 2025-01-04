5d2c114e4014cb49cbee764ba4d978852ac1bc0c
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2025-01-05 01:07:05 +0200

    Remove styles from Login.jsx

--- client/src/componnents/login/Login.jsx
+++ client/src/componnents/login/Login.jsx
@@ -1,10 +1,8 @@
 import { Link, useNavigate } from 'react-router-dom';
-
 import { useLogin } from '../../hooks/useAuth';
 import { useForm } from '../../hooks/useForm';
 import { useState } from 'react';
-import styles from './Login.module.css'
-
+import styles from './Login.module.css';
 
 const initialValues = { email: '', password: '' };
 
@@ -21,70 +19,83 @@ export default function Login() {
             setError(err.message);
         }
     };
+
     const { values, changeHandler, submitHandler } = useForm(initialValues, loginHandler);
 
     return (
-        <>
-            <div className="container-fluid py-5">
-                <div className="container">
-                    <div className="row g-0">
-                        <div className="col-lg-7">
-                            <div className="bg-primary h-100 p-5">
-                                <form onSubmit={submitHandler}>
-                                    <div className="row g-3">
+        <div className={styles.loginContainer}>
+            <div className="container">
+                <div className="row g-0">
+                    <div className="col-lg-7">
+                        <div className={styles.formSection}>
+                            <form onSubmit={submitHandler}>
+                                <div className="row g-3">
+                                    <div className="col-12">
+                                        <label className={styles.labelClass} htmlFor="email">
+                                            Email:
+                                        </label>
+                                        <input
+                                            className={`form-control ${styles.formInput}`}
+                                            type="email"
+                                            name="email"
+                                            id="email"
+                                            value={values.email}
+                                            onChange={changeHandler}
+                                            placeholder="email@example.com"
+                                        />
+                                    </div>
 
+                                    <div className="col-12">
+                                        <label className={styles.labelClass} htmlFor="password">
+                                            Password:
+                                        </label>
+                                        <input
+                                            className={`form-control ${styles.formInput}`}
+                                            type="password"
+                                            name="password"
+                                            id="password"
+                                            value={values.password}
+                                            onChange={changeHandler}
+                                            placeholder="******"
+                                        />
+                                    </div>
+
+                                    {error && (
                                         <div className="col-12">
-                                            <label className={styles.labelClass} htmlFor="email">Email:</label>
-                                            <input
-                                                className="form-control bg-light border-0 px-4"
-                                                style={{ height: '55px' }}
-                                                type="email"
-                                                name="email"
-                                                id='email'
-                                                value={values.email}
-                                                onChange={changeHandler}
-                                                placeholder="email@example.com"
-                                            />
+                                            <p className={styles.errorMessage}>{error}</p>
                                         </div>
-                                        <div className="col-12">
-                                            <label className={styles.labelClass} htmlFor="password">Password:</label>
-                                            <input
-                                                className="form-control bg-light border-0 px-4"
-                                                style={{ height: '55px' }}
-                                                type="password"
-                                                name="password"
-                                                id='password'
-                                                value={values.password}
-                                                onChange={changeHandler}
-                                                placeholder="******"
-                                            />
-                                        </div>
-                                        {error && (
-                                            <p>
-                                                <span style={{ color: 'red', fontSize: "18px" }}>{error}</span>
-                                            </p>
-                                        )}
-                                        <div className="col-3">
-                                            <button className="btn btn-secondary w-100 py-3" type="submit">Login</button>
-                                        </div>
-                                        <p className="field">
-                                            <span className={styles.labelClass}>If you don't have profile click <Link style={{ fontSize: "1em", color: '#F6FFF2' }} to="/register">here</Link> !</span>
+                                    )}
+
+                                    <div className="col-3">
+                                        <button className={`btn btn-secondary ${styles.submitButton}`} type="submit">
+                                            Login
+                                        </button>
+                                    </div>
+
+                                    <div className="col-12">
+                                        <p>
+                                            <span className={styles.labelClass}>
+                                                If you don't have profile click{' '}
+                                                <Link to="/register" className={styles.registerLink}>
+                                                    here
+                                                </Link>
+                                                !
+                                            </span>
                                         </p>
                                     </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="col-lg-5">
-                            <div className="bg-secondary h-100 p-5">
-                                <h2 className="text-white mb-4" style={{ textAlign: 'center' }}>Login</h2>
-                                <div className="d-flex mb-4">
-                                    <p className="text-white mb-4" >This is login page</p>
                                 </div>
-                            </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-5">
+                        <div className={styles.infoSection}>
+                            <h2 className={styles.infoTitle}>Login</h2>
+                            <p className={styles.infoText}>This is login page</p>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
