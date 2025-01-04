a4d6fe6d491735a3eb68c6d3903acce0ced98f42
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2025-01-05 01:17:20 +0200

    Update validationUtils.js

--- client/src/utils/validationUtils.js
+++ client/src/utils/validationUtils.js
@@ -16,11 +16,11 @@ const VALIDATION_RULES = {
     },
     email: {
         required: true,
-        pattern: /^[A-Z0_9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
     },
     password: {
         required: true,
-        minLength: 6
+        minLength: 4,
     },
     comment: {
         required: true,
@@ -29,83 +29,73 @@ const VALIDATION_RULES = {
 };
 
 const VALIDATION_MESSAGES = {
-    required: field => `${field} is required field!`,
-    minLength: (field, length) => `${field} must be at least ${length} charecters long!`,
-    maxLength: (field, length) => `${field} cannot exceed ${length} characters.`,
+    required: field => `${field} is required`,
+    minLength: (field, length) => `${field} must be at least ${length} characters`,
+    maxLength: (field, length) => `${field} cannot exceed ${length} characters`,
     pattern: {
         imageUrl: 'Please enter a valid URL',
         email: 'Please enter a valid email address',
     },
     passwordMatch: 'Passwords do not match',
-}
+};
 
 // Validation functions
 export const validateField = (name, value, rules = {}) => {
-    const errors = {};
+    const fieldErrors = [];
 
-    if (rules.required && !value.trim()) {
-        errors.push(VALIDATION_MESSAGES.required(name));
-    };
+    if (rules.required && !value?.trim()) {
+        fieldErrors.push(VALIDATION_MESSAGES.required(name));
+    }
 
-    if (rules.minLength && value?.length < rules?.minLength) {
-        errors.push(VALIDATION_MESSAGES.minLength(name, rules.maxLength));
-    };
+    if (value && rules.minLength && value.length < rules.minLength) {
+        fieldErrors.push(VALIDATION_MESSAGES.minLength(name, rules.minLength));
+    }
 
-    if (rules.maxLength && value?.length > rules.maxLength) {
-        errors.push(VALIDATION_MESSAGES.maxLength(name, rules.maxLength));
-    };
+    if (value && rules.maxLength && value.length > rules.maxLength) {
+        fieldErrors.push(VALIDATION_MESSAGES.maxLength(name, rules.maxLength));
+    }
 
-    if (rules.pattern && !rules.pattern.test(value)) {
-        errors.push(VALIDATION_MESSAGES.pattern[name]);
-    };
+    if (value && rules.pattern && !rules.pattern.test(value)) {
+        fieldErrors.push(VALIDATION_MESSAGES.pattern[name]);
+    }
 
-    return errors;
+    return fieldErrors;
 };
 
-// Form validations 
-export const validateProductForm = (values) => {
-    const errors = {};
-
-    const titleErrors = validateField('Title', values.title, VALIDATION_RULES.title);
-    if (titleErrors.length) {
-        errors.title = titleErrors;
-    };
-
-    const descriptionErrors = validateField('Description', values.description, VALIDATION_RULES.description);
-    if (descriptionErrors.length) {
-        errors.description = descriptionErrors;
-    };
-
-    const imageUrlErrors = validateField('Image Url', values.imageUrl, VALIDATION_RULES.imageUrl);
-    if (imageUrlErrors.length) {
-        errors.imageUrl = imageUrlErrors;
-    };
-
-    return errors;
-};
-
+// Form validators
 export const validateLoginForm = (values) => {
     const errors = {};
 
     const emailErrors = validateField('Email', values.email, VALIDATION_RULES.email);
-    if (emailErrors.length) {
-        errors.email = emailErrors;
-    };
+    if (emailErrors.length > 0) errors.email = emailErrors;
 
     const passwordErrors = validateField('Password', values.password, VALIDATION_RULES.password);
-    if (passwordErrors.length) {
-        errors.password = passwordErrors;
-    };
+    if (passwordErrors.length > 0) errors.password = passwordErrors;
 
     return errors;
 };
 
 export const validateRegisterForm = (values) => {
+    const errors = validateLoginForm(values);
+
+    if (values.password !== values.rePassword) {
+        errors.rePassword = [VALIDATION_MESSAGES.passwordMatch];
+    }
+
+    return errors;
+};
+
+export const validateProductForm = (values) => {
     const errors = {};
 
-    if (values.password != values.rePassword) {
-        errors.rePassword = [VALIDATION_MESSAGES.passwordMatch];
-    };
+    const titleErrors = validateField('Title', values.title, VALIDATION_RULES.title);
+    if (titleErrors.length > 0) errors.title = titleErrors;
+
+    const descriptionErrors = validateField('Description', values.description, VALIDATION_RULES.description);
+    if (descriptionErrors.length > 0) errors.description = descriptionErrors;
+
+    const imageUrlErrors = validateField('Image URL', values.imageUrl, VALIDATION_RULES.imageUrl);
+    if (imageUrlErrors.length > 0) errors.imageUrl = imageUrlErrors;
 
     return errors;
 };
@@ -113,10 +103,8 @@ export const validateRegisterForm = (values) => {
 export const validateCommentForm = (values) => {
     const errors = {};
 
-    const commentsErrors = validateField('Comment', values.comment, VALIDATION_RULES.comment);
-    if (commentsErrors.length) {
-        errors.comment = commentsErrors;
-    }
+    const commentErrors = validateField('Comment', values.comment, VALIDATION_RULES.comment);
+    if (commentErrors.length > 0) errors.comment = commentErrors;
 
     return errors;
-}
+};
